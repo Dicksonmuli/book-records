ba2a432dd891dd1d54cd92496a56779079ab230b
dicksonmuli <dicksonmully6@gmail.com>
2017-03-27 19:23:29 +0300

    new-book route- actions for the new-book route

--- app/components/new-book.js
+++ app/components/new-book.js
@@ -5,6 +5,18 @@ export default Ember.Component.extend({
   bookList: false;
 
   actions: {
-    
+    addNewBook(){
+      this.set('addBook', true)
+    }
+    addNewBook(){
+      this.set('bookList', true)
+    }
+    saveBook1(){
+      params = {
+        author: this.get('author') ? this.get('author'): '',
+        year: this.get('year') ? this.get('year'): '',
+        title: this.get('title') ? this.get('title'): '',
+      }
+    }
   }
 });
